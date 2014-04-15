8ceb7ffeb70d43be3df293e5e56530cb315b439f
Maxim Levicky <max.levicky@gmail.com>
2014-04-15 17:00:02 +0400

    IdEncode

--- src/main/java/co/uid/utils/IdEncoder.java
+++ src/main/java/co/uid/utils/IdEncoder.java
@@ -1,5 +1,6 @@
 package co.uid.utils;
 
+
 /**
  * @author Maxim Levicky
  *         created 14.04.2014
@@ -8,18 +9,29 @@ public class IdEncoder {
     public static char[] symbols = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-    public static String encodeId(Long id) {
+    public static String encodeId(long id) {
         if (id < 0) {
             throw new IllegalArgumentException("Id can't be less then 0");
         }
         StringBuilder encodedId = new StringBuilder();
+        int step = 1;
         do {
-            int i = ((int) (id % (symbols.length))) - 1;
-            encodedId.append(symbols[i == -1 ? symbols.length-1 : i]);
+            int modulo = (int) (id % (symbols.length));
+            if (step > 1) {
+                modulo--;
+                id--;
+            }
+            step++;
+            int resultChar = (modulo < 0) ? symbols.length + modulo : modulo;
+            encodedId.append(symbols[resultChar]);
             id = id / symbols.length;
         }
         while (id > 0);
-//        char[] stringId = String.valueOf(id).toCharArray();
+        return encodedId.reverse().toString();
+    }
+
+    public static Long decodeId(String encodedId) {
+        //        char[] stringId = String.valueOf(id).toCharArray();
 //        int currentPosition = stringId.length - 1;
 //        StringBuilder encodedId = new StringBuilder();
 //        for (char idChar : stringId) {
@@ -28,10 +40,6 @@ public class IdEncoder {
 //                encodedId.append(symbols[(int) (intCharId * Math.pow(10, currentPosition--)) - 1]);
 //            }
 //        }
-        return encodedId.reverse().toString();
-    }
-
-    public static Long decodeId(String encodedId) {
         return 0L;
     }
 }
